44285680791cbed9314f8187a1e5e23a2de73c99
Khaleel Gibran <khalby786@gmail.com>
2020-06-15 06:49:04 +0000

    new poll command

---
+++ commands/poll.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'poll',
+    description: 'Create a poll for users to vote',
+    args: true,
+    usage: '<poll message>',
+    cooldown: 60,
+    async execute(message, args) {
+
+        // only "manage server" users can use this command
+        if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("Insufficient permissions to run this command!");
+
+        const poll = new Discord.MessageEmbed()
+            .setTitle(`Poll by ${message.author.username}`)
+            .setColor(`rgb(66, 245, 158)`)
+            .setFooter("React to vote!")
+            .setDescription(args.join(' '));
+
+        let msg = await message.channel.send(poll);
+
+        // react
+        await msg.react(":white_check_mark:");
+        await msg.react(":negative_squared_cross_mark:");
+
+        // delete original message
+        message.delete();
+    }
+}
